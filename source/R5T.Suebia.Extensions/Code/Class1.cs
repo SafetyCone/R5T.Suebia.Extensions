251061a595137f8d7d4a6ccd1f93d11bf20f1ca6
MinexAutomation <minex.automation@gmail.com>
2019-12-13 12:35:38 -0800

    Added an IConfigurationBuilder extension method to add a secrets file using application configuration services.

--- source/R5T.Suebia.Extensions/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Suebia.Extensions
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Suebia.Extensions/Code/Extensions/IConfigurationBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace R5T.Suebia.Extensions
+{
+    public static class IConfigurationBuilderExtensions
+    {
+        public static IConfigurationBuilder AddJsonSecretsFilePath(this IConfigurationBuilder configurationBuilder, IServiceProvider configurationServiceProvider, string fileName)
+        {
+            var secretsFilePathProvider = configurationServiceProvider.GetRequiredService<ISecretsFilePathProvider>();
+
+            var filePath = secretsFilePathProvider.GetSecretsFilePath(fileName);
+
+            configurationBuilder.AddJsonFile(filePath);
+
+            return configurationBuilder;
+        }
+    }
+}
