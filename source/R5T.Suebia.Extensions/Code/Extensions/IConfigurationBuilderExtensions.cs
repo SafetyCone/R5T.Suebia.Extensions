0602694920f56522522a8b47fe8eccd3bbd56663
MinexAutomation <minex.automation@gmail.com>
2020-07-02 15:52:39 -0700

    Added documentation.

--- source/R5T.Suebia.Extensions/Code/Extensions/IConfigurationBuilderExtensions.cs
+++ source/R5T.Suebia.Extensions/Code/Extensions/IConfigurationBuilderExtensions.cs
@@ -8,6 +8,9 @@ namespace R5T.Suebia.Extensions
 {
     public static class IConfigurationBuilderExtensions
     {
+        /// <summary>
+        /// Uses the <see cref="ISecretsDirectoryFilePathProvider"/> service.
+        /// </summary>
         public static IConfigurationBuilder AddJsonSecretsFilePath(this IConfigurationBuilder configurationBuilder, IServiceProvider configurationServiceProvider, string fileName)
         {
             var secretsDirectoryFilePathProvider = configurationServiceProvider.GetRequiredService<ISecretsDirectoryFilePathProvider>();
